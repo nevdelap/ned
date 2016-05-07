b697b05f511601eaaf1ad66cda81f4aaaa3f01bc
Nev Delap <nevdelap@gmail.com>
2016-05-08 00:29:59 +1000

    A bit more.

--- src/source.rs
+++ src/source.rs
@@ -7,12 +7,7 @@ extern crate walkdir;
 use std::fs::File;
 #[cfg(test)]
 use std::io::Cursor;
-use std::io::{self, Read, Seek, SeekFrom, Write};
-use std::iter::Iterator;
-use std::path::PathBuf;
-use std::string::String;
-use std::{env, path, process};
-use walkdir::{WalkDir, WalkDirIterator};
+use std::io::Read;
 
 pub enum Source {
     Stdin(Box<Read>),
