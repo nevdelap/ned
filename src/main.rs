d2dfaf56e16db444f53aa62a8a94cd3fb0aeb0cf
Nev Delap <nevdelap@gmail.com>
2016-05-08 13:28:44 +1000

    A bit more.

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,14 @@ extern crate regex;
 extern crate walkdir;
 
 mod files;
+mod opts;
 mod parameters;
 mod source;
 
 use ansi_term::Colour::Red;
 use files::Files;
 use getopts::{Options, ParsingStyle};
+use opts::{make_opts, usage_version, usage_full};
 use parameters::{get_parameters, Parameters};
 use source::Source;
 use std::fs::OpenOptions;
@@ -73,20 +75,12 @@ fn ned(program: &str, args: &Vec<String>, mut output: &mut Write) -> Result<i32,
     let parameters = try!(get_parameters(&opts, args));
 
     if parameters.version {
-        println!("{}{}", &VERSION, &LICENSE);
+        println!("{}", usage_version());
         process::exit(1);
     }
 
     if parameters.globs.len() == 0 && !parameters.re.is_none() || parameters.help {
-        let brief = format!("Usage: {} {}\n{}",
-                            program,
-                            &OPTS_AND_ARGS,
-                            &PRE_DESCRIPTION);
-        println!("\n{}{}{}{}",
-                 opts.usage(&brief),
-                 &POST_DESCRIPTION,
-                 &VERSION,
-                 &LICENSE);
+        println!("{}", usage_full(program, &opts));
         process::exit(1);
     }
 
@@ -96,86 +90,6 @@ fn ned(program: &str, args: &Vec<String>, mut output: &mut Write) -> Result<i32,
     } else {
         1
     })
-}
-
-static OPTS_AND_ARGS: &'static str = "[OPTION]... [-p] <PATTERN> [FILE]...";
-static PRE_DESCRIPTION: &'static str = "
-ned is a bit like grep and a bit like sed, but not really. FILEs are ascii or
-utf-8 text files.
-
-For regex syntax see: http://rust-lang-nursery.github.io/regex/regex/#syntax";
-static POST_DESCRIPTION: &'static str = "
-Environment:
-    NED_DEFAULTS        ned options prepended to the program's arguments
-
-Exit codes:
-    0                   matches found/replaced
-    1                   no matches
-
-Quiet:
-    When -q --quiet is  specified ned tests for matches and returns an exit
-    code of 0 if a match is found in ANY file. When -a --all is combined with
-    quiet it returns an exit code of 0 if a match is found in ALL files. Quiet
-    any matches will only read only as many files as needed to find a match.
-    Quiet matches are more performant than non-quiet matches.
-";
-static VERSION: &'static str = "
-ned 0.1.0 Copyright (C) 2016 Nev Delap - https://github.com/nevdelap/ned
-";
-static LICENSE: &'static str = "
-License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
-This is free software: you are free to change and redistribute it.
-There is NO WARRANTY, to the extent permitted by law.
-";
-
-fn make_opts() -> Options {
-    let mut opts = Options::new();
-    opts.parsing_style(ParsingStyle::FloatingFrees);
-    opts.optopt("p",
-                "pattern",
-                "specify pattern, if the option isn't used the pattern must precede the files, \
-                 the option allows the pattern to be put after the files for more convenient \
-                 editing",
-                "PATTERN");
-    opts.optopt("r",
-                "replace",
-                "replace matches, may include named groups",
-                "REPLACEMENT");
-    // opts.optopt("n", "number", "match/replace N occurrences", "N");
-    // opts.optopt("k",
-    // "skip",
-    // "skip N occurrences before matching/replacing",
-    // "N");
-    // opts.optflag("b",
-    // "backwards",
-    // "-n --number and -k --skip options count backwards");
-    opts.optflag("i", "ignore-case", "ignore case");
-    opts.optflag("s",
-                 "single",
-                 ". matches newlines, ^ and $ match beginning and end of each file");
-    opts.optflag("m",
-                 "multiline",
-                 "multiline, ^ and $ match beginning and end of each line");
-    opts.optflag("l", "line-oriented", "operate on lines");
-    opts.optflag("x", "extended", "ignore whitespace and # comments");
-    opts.optflag("o", "only-matches", "show only matches");
-    opts.optopt("g",
-                "group",
-                "show the match group, specified by number or name",
-                "GROUP");
-    opts.optflag("v", "no-match", "show only non-matching");
-    opts.optflag("R", "recursive", "recurse");
-    opts.optflag("f", "follow", "follow symlinks");
-    opts.optmulti("", "include", "match only files that match GLOB", "GLOB");
-    opts.optmulti("", "exclude", "skip files matching GLOB", "GLOB");
-    opts.optmulti("", "exclude-dir", "skip directories matching GLOB", "GLOB");
-    opts.optflag("c", "colors", "show matches in color");
-    opts.optflag("", "stdout", "output to stdout");
-    opts.optflag("q", "quiet", "suppress all normal output");
-    opts.optflag("a", "all", "do not ignore entries starting with .");
-    opts.optflag("V", "version", "output version information and exit");
-    opts.optflag("h", "help", "print this help menu and exit");
-    opts
 }
 
 fn process_files(parameters: &Parameters, mut output: &mut Write) -> Result<bool, String> {
--- src/test_files.rs
+++ src/test_files.rs
@@ -1,6 +1,6 @@
 /// Test file related functionality - recursion, inclusion, exclusion, symlinks, etc.
 
-use make_opts;
+use opts::make_opts;
 use parameters::get_parameters;
 use files::Files;
 
