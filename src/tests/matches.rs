8f34a5d595723d814885d67216b8e2c8ccd86dd5
Nev Delap <nevdelap@gmail.com>
2016-05-27 19:31:45 +1000

    A bit more.

--- src/tests/matches.rs
+++ src/tests/matches.rs
@@ -751,7 +751,27 @@ This
 
 #[test]
 fn group_1_match_quiet_and_not_quiet() {
-    // DONE?
+
+    let input = "This is a test. This is a test.";
+    let pattern = "Th(is)";
+    let args = "--group 1";
+    let expected_found_matches = true;
+    let expected_screen_output = "\
+bogus_file.txt: is
+is
+";
+    let expected_file_content = &input;
+
+    test(input,
+         pattern,
+         args,
+         expected_found_matches,
+         expected_screen_output,
+         expected_file_content);
+}
+
+#[test]
+fn group_1_match_whole_files_quiet_and_not_quiet() {
 
     let input = "This is a test. This is a test.";
     let pattern = "Th(is)";
