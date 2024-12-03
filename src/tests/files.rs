ff43b6bfb8f0e2b4f8e6c6bf14140e6ebb6ea4cb
Nev Delap <nevdelap@gmail.com>
2024-12-04 09:28:34 +1000

    Improve file tests by making them check the whole paths.

--- src/tests/files.rs
+++ src/tests/files.rs
@@ -24,198 +24,221 @@ use crate::options_with_defaults::OptionsWithDefaults;
 use crate::opts::make_opts;
 use crate::parameters::get_parameters;
 use std::env;
+use std::path::{Path, PathBuf};
+
+// These tests are not running `ned` to find a pattern in the test files, they
+// are simulating so to test `Files`. To compare the results of running `ned` to
+// running the tests run `ned` with '.*' in place of 'pattern' in `args`, and
+// with `-f` to show only the files. For example `ned -f '.*' test` will give
+// the same results as the first test, no_recursion.
 
 #[test]
 fn no_recursion() {
-    let args = "pattern test";
-    let mut expected_file_names = vec!["file1.txt", "file9.txt", "longfile.txt"];
+    let test_path = Path::new("test");
+    let mut expected_file_names = vec![
+        test_path.join("file1.txt"),
+        test_path.join("file9.txt"),
+        test_path.join("longfile.txt"),
+    ];
     if cfg!(windows) {
         // Windows presents the symlink as a regular file.
-        expected_file_names.insert(1, "file8.txt");
+        expected_file_names.insert(1, test_path.join("file8.txt"));
     }
-
-    test(args, &expected_file_names);
+    test("pattern test", &expected_file_names);
+    // A leading ./ results in the same normalised relative paths.
+    test("pattern ./test", &expected_file_names);
 }
 
 #[test]
 fn no_recursion_all() {
-    let args = "pattern --all test";
-    let mut expected_file_names = vec![".hidden_file1", "file1.txt", "file9.txt", "longfile.txt"];
+    let test_path = Path::new("test");
+    let mut expected_file_names = vec![
+        test_path.join(".hidden_file1"),
+        test_path.join("file1.txt"),
+        test_path.join("file9.txt"),
+        test_path.join("longfile.txt"),
+    ];
     if cfg!(windows) {
         // Windows presents the symlink as a regular file.
-        expected_file_names.insert(2, "file8.txt");
+        expected_file_names.insert(2, test_path.join("file8.txt"));
     }
-
-    test(args, &expected_file_names);
+    test("pattern --all test", &expected_file_names);
 }
 
 #[test]
 fn no_recursion_follow() {
-    let args = "pattern --follow test";
-    let expected_file_names = ["file1.txt", "file8.txt", "file9.txt", "longfile.txt"];
-
-    test(args, &expected_file_names);
+    let test_path = Path::new("test");
+    let expected_file_names = vec![
+        test_path.join("file1.txt"),
+        test_path.join("file8.txt"),  // TEST ON WINDOWS
+        test_path.join("file9.txt"),
+        test_path.join("longfile.txt"),
+    ];
+    test("pattern --follow test", &expected_file_names);
+    // A leading ./ results in the same normalised relative paths.
+    test("pattern --follow ./test", &expected_file_names);
 }
 
 #[test]
 fn no_recursion_follow_all() {
-    let args = "pattern --follow --all test";
-    let expected_file_names = [
-        ".hidden_file1",
-        "file1.txt",
-        "file8.txt",
-        "file9.txt",
-        "longfile.txt",
+    let test_path = Path::new("test");
+    let expected_file_names = vec![
+        test_path.join(".hidden_file1"),
+        test_path.join("file1.txt"),
+        test_path.join("file8.txt"),
+        test_path.join("file9.txt"),
+        test_path.join("longfile.txt"),
     ];
-
-    test(args, &expected_file_names);
+    test("pattern --follow --all test", &expected_file_names);
 }
 
 #[test]
 fn recursion() {
-    let args = "pattern --recursive test";
+    let test_path = Path::new("test");
     let mut expected_file_names = vec![
-        "file1.txt",
-        "file2.txt",
-        "file3.txt",
-        "file4.txt",
-        "file5.txt",
-        "file6.txt",
-        "file7.txt",
-        "file9.txt",
-        "longfile.txt",
+        test_path.join("dir1").join("dir4").join("dir5").join("file7.txt"),
+        test_path.join("dir1").join("dir4").join("file6.txt"),
+        test_path.join("dir1").join("file2.txt"),
+        test_path.join("dir1").join("file3.txt"),
+        test_path.join("dir2").join("file4.txt"),
+        test_path.join("dir3").join("file5.txt"),
+        test_path.join("file1.txt"),
+        test_path.join("file9.txt"),
+        test_path.join("longfile.txt"),
     ];
     if cfg!(windows) {
         // Windows presents the symlink as a regular file.
-        expected_file_names.insert(7, "file8.txt");
+        expected_file_names.insert(7, test_path.join("file8.txt"));
     }
-
-    test(args, &expected_file_names);
+    test("pattern --recursive test", &expected_file_names);
 }
 
 #[test]
 fn recursion_all() {
-    let args = "pattern --recursive --all test";
+    let test_path = Path::new("test");
     let mut expected_file_names = vec![
-        ".hidden_file1",
-        ".hidden_file2",
-        "file1.txt",
-        "file10.txt",
-        "file2.txt",
-        "file3.txt",
-        "file4.txt",
-        "file5.txt",
-        "file6.txt",
-        "file7.txt",
-        "file9.txt",
-        "longfile.txt",
+        test_path.join(".hidden_dir").join(".hidden_file2"),
+        test_path.join(".hidden_dir").join("file10.txt"),
+        test_path.join(".hidden_file1"),
+        test_path.join("dir1").join("dir4").join("dir5").join("file7.txt"),
+        test_path.join("dir1").join("dir4").join("file6.txt"),
+        test_path.join("dir1").join("file2.txt"),
+        test_path.join("dir1").join("file3.txt"),
+        test_path.join("dir2").join("file4.txt"),
+        test_path.join("dir3").join("file5.txt"),
+        test_path.join("file1.txt"),
+        test_path.join("file9.txt"),
+        test_path.join("longfile.txt"),
     ];
     if cfg!(windows) {
         // Windows presents the symlink as a regular file.
-        expected_file_names.insert(9, "file8.txt");
+        expected_file_names.insert(10, test_path.join("file8.txt"));
     }
-
-    test(args, &expected_file_names);
+    test("pattern --recursive --all test", &expected_file_names);
 }
 
 #[test]
 fn recursion_follow() {
-    let args = "pattern --follow test";
-    let expected_file_names = ["file1.txt", "file8.txt", "file9.txt", "longfile.txt"];
-
-    test(args, &expected_file_names);
+    let test_path = Path::new("test");
+    let expected_file_names = vec![
+        test_path.join("file1.txt"),
+        test_path.join("file8.txt"),
+        test_path.join("file9.txt"),
+        test_path.join("longfile.txt"),
+    ];
+    test("pattern --follow test", &expected_file_names);
 }
 
 #[test]
 fn recursion_follow_all() {
-    let args = "pattern --recursive --follow --all test";
-    let expected_file_names = [
-        ".hidden_file1",
-        ".hidden_file2",
-        "file1.txt",
-        "file10.txt",
-        "file2.txt",
-        "file3.txt",
-        "file4.txt",
-        "file5.txt",
-        "file6.txt",
-        "file7.txt",
-        "file8.txt",
-        "file9.txt",
-        "longfile.txt",
+    let test_path = Path::new("test");
+    let expected_file_names = vec![
+        test_path.join(".hidden_dir").join(".hidden_file2"),
+        test_path.join(".hidden_dir").join("file10.txt"),
+        test_path.join(".hidden_file1"),
+        test_path.join("dir1").join("dir4").join("dir5").join("file7.txt"),
+        test_path.join("dir1").join("dir4").join("file6.txt"),
+        test_path.join("dir1").join("file2.txt"),
+        test_path.join("dir1").join("file3.txt"),
+        test_path.join("dir2").join("file4.txt"),
+        test_path.join("dir3").join("file5.txt"),
+        test_path.join("file1.txt"),
+        test_path.join("file8.txt"),
+        test_path.join("file9.txt"),
+        test_path.join("longfile.txt"),
     ];
-
-    test(args, &expected_file_names);
+    test("pattern --recursive --follow --all test", &expected_file_names);
 }
 
 #[test]
 fn include_files() {
-    let args = "pattern -R test --include file7*";
-    let expected_file_names = ["file7.txt"];
-
-    test(args, &expected_file_names);
+    let test_path = Path::new("test");
+    let expected_file_names = vec![test_path.join("dir1").join("dir4").join("dir5").join("file7.txt")];
+    test("pattern -R test --include file7*", &expected_file_names);
 }
 
 #[test]
 fn exclude_files() {
+    let test_path = Path::new("test");
     let args = "pattern -R test --exclude file7*";
     let mut expected_file_names = vec![
-        "file1.txt",
-        "file2.txt",
-        "file3.txt",
-        "file4.txt",
-        "file5.txt",
-        "file6.txt",
-        "file9.txt",
-        "longfile.txt",
+        test_path.join("dir1").join("dir4").join("file6.txt"),
+        test_path.join("dir1").join("file2.txt"),
+        test_path.join("dir1").join("file3.txt"),
+        test_path.join("dir2").join("file4.txt"),
+        test_path.join("dir3").join("file5.txt"),
+        test_path.join("file1.txt"),
+        test_path.join("file9.txt"),
+        test_path.join("longfile.txt"),
     ];
     if cfg!(windows) {
         // Windows presents the symlink as a regular file.
-        expected_file_names.insert(6, "file8.txt");
+        expected_file_names.insert(6, test_path.join("file8.txt"));
     }
-
     test(args, &expected_file_names);
 }
 
 #[test]
 fn exclude_directory() {
+    let test_path = Path::new("test");
     let args = "pattern -R test --exclude-dir dir4";
     let mut expected_file_names = vec![
-        "file1.txt",
-        "file2.txt",
-        "file3.txt",
-        "file4.txt",
-        "file5.txt",
-        "file9.txt",
-        "longfile.txt",
+        test_path.join("dir1").join("file2.txt"),
+        test_path.join("dir1").join("file3.txt"),
+        test_path.join("dir2").join("file4.txt"),
+        test_path.join("dir3").join("file5.txt"),
+        test_path.join("file1.txt"),
+        test_path.join("file9.txt"),
+        test_path.join("longfile.txt"),
     ];
     if cfg!(windows) {
         // Windows presents the symlink as a regular file.
-        expected_file_names.insert(5, "file8.txt");
+        expected_file_names.insert(5, test_path.join("file8.txt"));
     }
-
     test(args, &expected_file_names);
 }
 
-fn test(args: &str, expected_file_names: &[&str]) {
+fn test(args: &str, expected_file_names: &Vec<PathBuf>) {
     let args = args
         .split_whitespace()
         .map(|arg| arg.to_string())
         .collect::<Vec<String>>();
+
     unsafe { env::set_var("NED_DEFAULTS", "") };
+
     let options_with_defaults = OptionsWithDefaults::new(make_opts(), &args).unwrap();
     let parameters = get_parameters(&options_with_defaults).unwrap();
-    let paths = Files::new(&parameters, &parameters.globs[0]);
-    let mut file_names = paths
-        .map(|path| path.file_name().unwrap().to_str().unwrap().to_string())
+
+    let file_names = Files::new(&parameters, &parameters.globs[0]);
+
+    let mut file_names = file_names
+        .map(|path| path.as_path().to_string_lossy().to_string())
         .collect::<Vec<String>>();
     file_names.sort();
 
-    assert_eq!(
-        &file_names,
-        &expected_file_names
-            .iter()
-            .map(|file_name| file_name.to_string())
-            .collect::<Vec<String>>()
-    );
+    let expected_file_names = expected_file_names.iter()
+        .map(|path| path.to_str().unwrap().to_string())
+        .collect::<Vec<String>>();
+
+    assert_eq!(&file_names, &expected_file_names);
 }
