39e8391cbe6528ab7f7d298dee92ba01e9c4d519
Nev Delap <nevdelap@gmail.com>
2024-12-04 17:09:02 +1000

    Run tests in debug and release in build scripts.

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,6 +1,8 @@
 :: Run from the project root.
 @echo off
-cargo build --release ^
+cargo test ^
+    && cargo test -- release ^
+    && cargo build --release ^
     && gzip --stdout target/release/ned.exe > artifacts/ned.windows.gz ^
     || exit /b 1
 echo Artifacts written to ./artifacts.
